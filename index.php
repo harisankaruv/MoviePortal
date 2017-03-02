136f57ae3263ed48592f6fcfb5d1da9db9e220d1
haris <haris@DESKTOP-192CT34>
2017-03-02 20:55:23 +0530

    commit 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div>
+            <form>
+                <input type="text" name="username" placeholder="Username"/><br>
+                <input type="password" name="upass" placeholder="Password"/><br>
+                <input type="button" value="Log In" onclick="fn()"/>
+                <input type="button" value="Sign Up" onclick="fn2()"/>
+            </form>
+        </div>
+    </body>
+</html>
