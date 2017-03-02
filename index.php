4d0650d3de9955bfc4eba3fddf56724ac83d563a
haris <haris@DESKTOP-192CT34>
2017-03-02 21:05:46 +0530

    commit 1

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="loginStyle.css">
     </head>
     <body>
         <?php
@@ -17,8 +18,10 @@ and open the template in the editor.
             <form>
                 <input type="text" name="username" placeholder="Username"/><br>
                 <input type="password" name="upass" placeholder="Password"/><br>
-                <input type="button" value="Log In" onclick="fn()"/>
-                <input type="button" value="Sign Up" onclick="fn2()"/>
+                <div>
+                    <input type="button" value="Log In" onclick="fn()"/>
+                    <input type="button" value="Sign Up" onclick="fn2()"/>
+                <div>
             </form>
         </div>
     </body>
