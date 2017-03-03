940f8323d2929f239044bb97673e08437eab71dc
haris <haris@DESKTOP-192CT34>
2017-03-03 20:21:34 +0530

    Login Finalized with optional change for background image

--- signin.php
+++ signin.php
@@ -26,8 +26,8 @@ and open the template in the editor.
                 <div class="w3-container w3-padding-32">
                     <input type="text" name="username" class="w3-input" style="background-color: transparent"/>
                     <label class="w3-label w3-validate">Username</label>
-                    <input type="password" name="upass" class="w3-input"/>
-                    <label class="w3-label w3-validate" style="background-color: transparent">Password</label>
+                    <input type="password" name="upass" class="w3-input" style="background-color: transparent"/>
+                    <label class="w3-label w3-validate"">Password</label>
                 </div>
                 <div class="w3-container w3-center w3-padding-12">
                     <input type="button" value="Log In" onclick="fn()" class="w3-button w3-teal w3-round-large"/>
