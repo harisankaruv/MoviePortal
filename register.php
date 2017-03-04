872cb5ae157d77dece5509ee0a463f0c21210087
haris <haris@DESKTOP-192CT34>
2017-03-04 22:06:50 +0530

    Added sign up page, removed body background. Added a readme file via github to add change details. Almost finalized the registration page (need to add function for adding profile picture).

--- register.php
+++ register.php
@@ -18,23 +18,23 @@
                 <div class="w3-container w3-blue">
                     <h2 class="w3-animate-right">Sign Up</h2>
                 </div>
-                <div class="w3-container w3-padding-12">
-                    <input type="text" name="fname" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 50%"/>
+                <div class="w3-container w3-padding-12" style="width: 60%">
+                    <input type="text" name="fname" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 90%"/>
                     <label class="w3-label">First Name</label>
-                    <input type="text" name="lname" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 50%"/>
+                    <input type="text" name="lname" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 90%"/>
                     <label class="w3-label">Last Name</label>
-                    <input type="email" name="mailid" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 50%"/>
+                    <input type="email" name="mailid" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 90%"/>
                     <label class="w3-label">E-mail</label>
-                    <input type="text" name="pass-main" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 50%"/>
+                    <input type="text" name="pass-main" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 90%"/>
                     <label class="w3-label">Password</label>
-                    <input type="text" name="pass-confirm" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 50%"/>
+                    <input type="text" name="pass-confirm" class="w3-input w3-animate-input w3-hover-teal" style="background-color: transparent; width: 90%"/>
                     <label class="w3-label">Confirm Password</label>
                     <br><br>
                     <input type="checkbox" class="w3-check" name="policy"/>
                     <label class="w3-label">I accept to the terms and policies</label><br><br>
-                    <div class="w3-center">
-                        <input type="button" class="w3-button w3-teal w3-round-large w3-ripple" value="Sign up" onclick="fn()"/>
-                    </div>
+                </div>
+                <div class="w3-center w3-padding-12">
+                    <input type="button" class="w3-button w3-teal w3-round-large w3-ripple" value="Sign up" onclick="fn()"/>
                 </div>
             </form>
         </div>
